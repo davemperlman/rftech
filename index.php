a93f1ed6c0e7dbf705798e6f65053b89e48e5985
Dave Perlman <daveperlman89@me.com>
2017-07-22 16:17:21 -0400

    making progress

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<title>Refined Technologies</title>
 	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
 	<link rel="stylesheet" href="css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Cabin:300,400,500,700" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
@@ -36,6 +37,13 @@
 		<span>Get Started</span>
 	</div>
 </section>
+<section class="block contact">
+	<h3>Contact</h3>
+	<ul>
+		<li>hi</li>
+		<li>hello</li>
+	</ul>
+</section>
 <div class="info-wrap">
 	<section class="block about">
 		<h3>About us</h3>
@@ -45,10 +53,10 @@
 		<div>
 			<h3>Redefine | Refine</h3>
 			<p>Conceptualizing an internet presence can be an arduos task. What do I want to say - who do I want to say it to - how do I want to say it - These are all questions that we help you to answer. A site isn't just a site anymore. What your site offers will define you or your company in the eyes of your users.</p>
-			<img src="http://via.placeholder.com/200x200" alt="">
+			<img src="img/lightbulb.png" alt="">
 			<h3>Modern Design Practice</h3>
 			<p>The competitive market demands a business be fluid and ready to change. Utlizing the latest development and design trends keeps your web presence fresh and boasts a refreshing user experience for your clients.</p>
-			<img src="http://via.placeholder.com/200x200" alt="">
+			<img src="img/design.png" alt="">
 		</div>
 	</section>
 </div>
@@ -59,22 +67,22 @@
 			<li>
 				<h4>Design</h4>
 				<p>Provide your users with a sleek, modern visual experience that allows them to easily navigate. It's a creative process, no two designs are the same. We build our designs based on what you want the user experience to be.</p>
-				<img src="http://via.placeholder.com/200x200" alt="">
+				<img src="img/design2.png" alt="">
 			</li>
 			<li>
 				<h4>Data</h4>
 				<p>The processing and manipulation of data is an often overlooked aspect of modern web applications. Contracts, email lists, customer databases, and the like are all dynamic information that can often be more efficiently managed by a web application. We can help you increase the interactivity of your site by building custom data management and processing functionality.</p>
-				<img src="http://via.placeholder.com/200x200" alt="">
+				<img src="img/data.png" alt="">
 			</li>
 			<li>
 				<h4>Content Management</h4>
 				<p>Managing content. As an administrator of a site, you should be able to easily modify and update content on your site. Add a blog post, upload media, change the title of an article, etc. These are all things that can be done without having to contact a developer. Keep more of your time.</p>
-				<img src="http://via.placeholder.com/200x200" alt="">
+				<img src="img/wordpress.png" alt="">
 			</li>
 			<li>
 				<h4>SEO</h4>
 				<p>So you have a site, great! Now let's help people find it. There are BILLIONS of websites on the internet, we make sure that people are finding yours using proper semantics and targeted keywords.</p>
-				<img src="http://via.placeholder.com/200x200" alt="">
+				<img src="img/seo.png" alt="">
 			</li>
 		</ul>
 	</section>

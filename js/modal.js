1870cd000b1e618795a3ca9578b1bdb66ba525f1
Dave Perlman <daveperlman89@me.com>
2017-08-08 21:01:07 -0400

    fixed mobile menu height

--- js/modal.js
+++ js/modal.js
@@ -26,20 +26,17 @@
 		var subject = $('#subject').val();
 		var message = $('#message').val();
 
-		if ($('#send').attr('inactive') !== 'undefined') {
-			console.log('stuff');
-		}else{
-			$('.modal #loading-img').show();
-			$.ajax({
-				url: 'send.php',
-				type: "POST",
-				cache: false,
-				data: {email, subject, message},
-				success: function(html) {
-					$('.modal #loading-img').hide();
-					$('#status').html(html);
-					$('.modal-wrap').delay(1000).fadeOut();
-				}
-			});
-		};
+		$('.modal #loading-img').show();
+		$.ajax({
+			url: 'send.php',
+			type: "POST",
+			cache: false,
+			data: {email, subject, message},
+			success: function(html) {
+				$('.modal #loading-img').hide();
+				$('#status').html(html);
+				$('.modal-wrap').delay(1000).fadeOut();
+			}
+		});
+	});
 }());
